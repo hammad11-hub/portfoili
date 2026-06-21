1170d66df37ce72d4942848ab1aa32d709fab791
hammad iqbal <hammad125100@gmil.com>
2026-06-20 17:12:22 -0700

    Convert frontend to static portfolio only for Netlify deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import './App.css';
 
@@ -13,32 +12,7 @@ const fallbackProjects = [
 ];
 
 function App() {
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    const apiUrl = process.env.REACT_APP_API_URL;
-
-    if (!apiUrl) {
-      setProjects(fallbackProjects);
-      return;
-    }
-
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        if (Array.isArray(data) && data.length > 0) {
-          setProjects(data);
-        } else {
-          setProjects(fallbackProjects);
-        }
-      })
-      .catch((error) => {
-        console.error('Failed to load projects:', error);
-        setProjects(fallbackProjects);
-      });
-  }, []);
-
-  return <Home projects={projects} />;
+  return <Home projects={fallbackProjects} />;
 }
 
 export default App;
